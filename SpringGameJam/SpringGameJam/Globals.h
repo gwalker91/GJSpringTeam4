399107e9def71b87628c8ff5347fda1e97b7ac2b
Grant Walker <gwwalker@uci.edu>
2014-04-05 01:34:29 -0700

    Changed the Globals.h

--- SpringGameJam/SpringGameJam/Globals.h
+++ SpringGameJam/SpringGameJam/Globals.h
@@ -7,4 +7,8 @@ extern int SCREEN_HEIGHT;
 
 extern int gravity;
 
+//Create a dictionary of textures to load
+//Then we can go through these and make them sprites when need be
+//extern Dictionary of Textures!
+
 #endif
